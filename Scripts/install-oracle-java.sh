9e76b5f2f47ccb5de84c589d27e27a515e7d12cc
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-06-09 22:52:05 +0530

    Update install-oracle-java.sh

--- Scripts/install-oracle-java.sh
+++ Scripts/install-oracle-java.sh
@@ -19,13 +19,12 @@ cd
 cd
 #install Tomcat
 mkdir /opt/tomcat
-cd /opt/tomcat
-wget http://www.interior-dsgn.com/apache/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.zip
-wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.zip.md5
-cat apache-tomcat-8.5.2.zip.md5
-md5sum apache-tomcat-8.5.2.zip
-unzip apache-tomcat-8.5.2.zip
-cd apache-tomcat-8.5.2/bin
+cd /opt/tomcat && wget http://www.interior-dsgn.com/apache/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.zip
+cd /opt/tomcat && wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.zip.md5
+cat /opt/tomcat/apache-tomcat-8.5.2.zip.md5
+md5sum /opt/tomcat/apache-tomcat-8.5.2.zip
+unzip /opt/tomcat/apache-tomcat-8.5.2.zip
+cd /opt/tomcat/apache-tomcat-8.5.2/bin
 chmod 700 /opt/tomcat/apache-tomcat-8.5.2/bin/*.sh
 cd /opt/tomcat/apache-tomcat-8.5.2/bin/ 
-./startup.sh
+sh /opt/tomcat/apache-tomcat-8.0.29/bin/startup.sh
