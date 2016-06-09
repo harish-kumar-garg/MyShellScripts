57f24af9e5e634d2b4c47c8abccd8358118eb883
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-06-10 02:55:40 +0530

    Update install-oracle-java.sh

--- Scripts/install-oracle-java.sh
+++ Scripts/install-oracle-java.sh
@@ -4,12 +4,12 @@ cd /opt/java
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
 tar -zxvf jdk-8u45-linux-x64.tar.gz
 cd jdk1.8.0_45/
-update-alternatives --install /usr/bin/java java /opt/java/jdk1.8.0_45/bin/java 100
-update-alternatives --config java
-update-alternatives --install /usr/bin/javac javac /opt/java/jdk1.8.0_45/bin/javac 100
-update-alternatives --config javac
-update-alternatives --install /usr/bin/jar jar /opt/java/jdk1.8.0_45/bin/jar 100
-update-alternatives --config jar
+#update-alternatives --install /usr/bin/java java /opt/java/jdk1.8.0_45/bin/java 100
+#update-alternatives --config java
+#update-alternatives --install /usr/bin/javac javac /opt/java/jdk1.8.0_45/bin/javac 100
+#update-alternatives --config javac
+#update-alternatives --install /usr/bin/jar jar /opt/java/jdk1.8.0_45/bin/jar 100
+#update-alternatives --config jar
 export JAVA_HOME=/opt/java/jdk1.8.0_45/
 export JRE_HOME=/opt/java/jdk1.8.0._45/jre
 export PATH=$PATH:/opt/java/jdk1.8.0_45/bin:/opt/java/jdk1.8.0_45/jre/bin
