3dcdf4367ed82b773a65e3630c25240e7b1c9330
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-07-04 14:51:26 +0530

    Delete simple-java-jenkins.sh

--- simple-java-jenkins.sh
+++
@@ -1,6 +0,0 @@
-sudo su
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-get update
-apt-get -y install default-jdk
-apt-get -y install jenkins
