b8206211378944c82f54f71c8cfde565afdc59bb
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-07-22 16:27:51 +0530

    Update delete_resource_group.sh

--- delete_resource_group.sh
+++ delete_resource_group.sh
@@ -1,6 +1,5 @@
-sudo su
 cd /etc/
-sh download.sh
+sudo sh download.sh
 cd
 #curl -X POST https://login.windows.net/85c997b9-f494-46b3-a11d-772983cf6f11/oauth2/token  \
  # -F redirect_uri=http://localhost \
