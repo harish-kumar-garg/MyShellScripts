6b322e2cadc65673f38e3c8522832c790201c17c
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-07-27 12:09:03 +0530

    Update delete_res_grp.sh

--- delete_res_grp.sh
+++ delete_res_grp.sh
@@ -21,8 +21,8 @@ delete_resource_group() {
 	TOTAL_NUMBER_OF_RG=
 	while [ "${I}" -ge 0 ]
 	do
-		NAME1='$(cat rgName.json | jq .value['$I'].name)'
-		if [ $NAME1 = 'null' ]; then
+		NAME1="$(cat rgNames.json | jq .value['$I'].name)"
+		if [ "${NAME1}" = 'null' ]; then
 			break;
 		else
 			I=`expr $I + 1`;
@@ -34,12 +34,12 @@ delete_resource_group() {
 	#date is in format ddmmyyyy
 	while [ "${I}" -lt "${TOTAL_NUMBER_OF_RG}" ]
 	do
-		NAME1='$(cat rgName.json | jq .value['$I'].name)'
-		if [ $NAME1 = *DND* ]; then
-				echo $NAME1' is not deleted'
+		NAME1="$(cat rgNames.json | jq .value['$I'].name)"
+		if [ "${NAME1}" = *DND* ]; then
+				echo "${NAME1}"' is not deleted'
 				I=`expr $I + 1`
 			else
-				echo $NAME1'will be deleted'
+				echo "${NAME1}"'will be deleted'
 				I=`expr $I + 1`
 		fi
 	done
