9c2d64304ae6a564ed3017609f70496190cd806c
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-08-09 13:45:39 +0530

    Update jenkins-mysql-sonar.sh

--- Scripts/jenkins-mysql-sonar.sh
+++ Scripts/jenkins-mysql-sonar.sh
@@ -27,15 +27,15 @@ sudo apt-get install -y unzip
 unzip sonarqube-5.6.zip
 sudo mv sonarqube-5.6 /opt/sonar
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-cat "sonar.jdbc.username=sonar" >> /opt/sonar/conf/sonar.properties
-cat "sonar.jdbc.password=sonar" >> /opt/sonar/conf/sonar.properties
-cat "sonar.jdbc.url=jdbc:mysql://{$ip}:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance" >>/opt/sonar/conf/sonar.properties
-cat "sonar.web.host=0.0.0.0" >> /opt/sonar/conf/sonar.properties
-cat "sonar.web.context=/sonar" >> /opt/sonar/conf/sonar.properties
-cat "sonar.web.port=9000" >> /opt/sonar/conf/sonar.properties
+echo 'sonar.jdbc.username=sonar' >> /opt/sonar/conf/sonar.properties
+echo 'sonar.jdbc.password=sonar' >> /opt/sonar/conf/sonar.properties
+echo 'sonar.jdbc.url=jdbc:mysql://{$ip}:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance' >>/opt/sonar/conf/sonar.properties
+echo 'sonar.web.host=0.0.0.0' >> /opt/sonar/conf/sonar.properties
+echo 'sonar.web.context=/sonar' >> /opt/sonar/conf/sonar.properties
+echo 'sonar.web.port=9000' >> /opt/sonar/conf/sonar.properties
 cp /opt/sonar/bin/linux-x86-64/sonar.sh /etc/init.d/sonar
-cat "SONAR_HOME=/opt/sonar" >> /etc/init.d/sonar
-cat "PLATFORM=linux-x86-64" >> /etc/init.d/sonar
+echo 'SONAR_HOME=/opt/sonar' >> /etc/init.d/sonar
+echo 'PLATFORM=linux-x86-64' >> /etc/init.d/sonar
 sed -i -e"s/^WRAPPER_CMD\s*=\s*./wrapper/WRAPPER_CMD = ${SONAR_HOME}/conf/wrapper.conf" /etc/init.d/sonar
 sed -i -e"s/^WRAPPER_CONF\s*=\s*../../conf/wrapper.conf/WRAPPER_CONF = ${SONAR_HOME}/conf/wrapper.conf" /etc/init.d/sonar
 sed -i -e"s/^PIDDIR\s*=\s*./PIDDIR = /var/run/" /etc/init.d/sonar
