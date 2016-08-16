9752d993f4f8799426e0a397a83e0b65a6a59206
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-08-16 13:11:33 +0530

    Update install-tomcat.sh

--- Scripts/install-tomcat.sh
+++ Scripts/install-tomcat.sh
@@ -1,12 +1,16 @@
 #install Tomcat
+sudo su
+apt-get update
+apt-get install -y default-jdk
+cd
 mkdir /opt/tomcat
 cd /opt/tomcat
-wget http://www.interior-dsgn.com/apache/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.zip
-wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.zip.md5
-cat apache-tomcat-8.5.2.zip.md5
-md5sum apache-tomcat-8.5.2.zip
-unzip apache-tomcat-8.5.2.zip
-cd apache-tomcat-8.5.2/bin
-chmod 700 /opt/tomcat/apache-tomcat-8.5.2/bin/*.sh
-cd /opt/tomcat/apache-tomcat-8.5.2/bin/ 
+wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.zip
+wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.zip.md5
+cat apache-tomcat-8.5.4.zip.md5
+md5sum apache-tomcat-8.5.4.zip
+unzip apache-tomcat-8.5.4.zip
+cd apache-tomcat-8.5.4/bin
+chmod 700 /opt/tomcat/apache-tomcat-8.5.4/bin/*.sh
+cd /opt/tomcat/apache-tomcat-8.5.4/bin/ 
 ./startup.sh
