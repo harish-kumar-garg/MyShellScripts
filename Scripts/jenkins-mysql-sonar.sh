bdafc01ddbe7c0f70154bd5a8e3f635456946f77
harish-kumar-garg <harish.garg@mindtree.com>
2016-09-12 00:51:32 +0530

    Update jenkins-mysql-sonar.sh
    Sonar update

--- Scripts/jenkins-mysql-sonar.sh
+++ Scripts/jenkins-mysql-sonar.sh
@@ -31,30 +31,33 @@ echo mysql-server-5.6 mysql-server/root_password_again password welcome123 | deb
 # install the LAMP stack
 sudo apt-get -y install mysql-server 
 
-sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
 sudo service mysql restart
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
 
 echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
-
-wget https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-5.6.zip
+# install sonarqube
+wget https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.0.zip
 sudo apt-get install -y unzip
-unzip sonarqube-5.6.zip
-sudo mv sonarqube-5.6 /opt/sonar
+unzip sonarqube-6.0.zip
+sudo mv sonarqube-6.0 /opt/sonar
 IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+echo '' >> /opt/sonar/conf/sonar.properties
 echo 'sonar.jdbc.username=sonar' >> /opt/sonar/conf/sonar.properties
 echo 'sonar.jdbc.password=sonar' >> /opt/sonar/conf/sonar.properties
 echo "sonar.jdbc.url=jdbc:mysql://${IP}:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance" >>/opt/sonar/conf/sonar.properties
 echo 'sonar.web.host=0.0.0.0' >> /opt/sonar/conf/sonar.properties
 echo 'sonar.web.context=/sonar' >> /opt/sonar/conf/sonar.properties
 echo 'sonar.web.port=9000' >> /opt/sonar/conf/sonar.properties
-cp /opt/sonar/bin/linux-x86-64/sonar.sh /etc/init.d/sonar
+sudo cp /opt/sonar/bin/linux-x86-64/sonar.sh /etc/init.d/sonar
+sudo chmod 777 /etc/init.d/sonar
+echo '' >> /etc/init.d/sonar
 echo 'SONAR_HOME='/opt/sonar'' >> /etc/init.d/sonar
 echo 'PLATFORM='linux-x86-64'' >> /etc/init.d/sonar
-sudo sed -i -e"s/^WRAPPER_CMD\s*=\s*./wrapper/WRAPPER_CMD = ${SONAR_HOME}/bin/${PLATFORM}/wrapper" /etc/init.d/sonar
-sudo sed -i -e"s/^WRAPPER_CONF\s*=\s*../../conf/wrapper.conf/WRAPPER_CONF = ${SONAR_HOME}/conf/wrapper.conf" /etc/init.d/sonar
-sudo sed -i -e"s/^PIDDIR\s*=\s*./PIDDIR = /var/run/" /etc/init.d/sonar
+sed -i 's/WRAPPER_CMD=".\/wrapper"/WRAPPER_CMD="\${SONAR_HOME}\/bin\/\${PLATFORM}\/wrapper"/g' /etc/init.d/sonar
+sudo sed -i -e' s/WRAPPER_CONF="..\/..\/conf\/wrapper.conf"/WRAPPER_CONF="\${SONAR_HOME}\/conf\/wrapper.conf"/g' /etc/init.d/sonar
+sudo sed -i -e 's/PIDDIR="."/PIDDIR="\/var\/run"/g' /etc/init.d/sonar
 sudo update-rc.d -f sonar remove
 sudo chmod 755 /etc/init.d/sonar
 sudo update-rc.d sonar defaults
