096aa52d1fbf383907e766fef0ebadaab2b8fa7b
harish-kumar-garg <harish.garg@mindtree.com>
2016-09-27 14:14:54 +0530

    Create hg-mysql-db.sh
    MySQL db installation.

---
+++ Scripts/hg-mysql-db.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+sudo apt-get update
+cd
+#installing mysql
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password welcome123 | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password welcome123 | debconf-set-selections
+# install the LAMP stack
+sudo apt-get -y install mysql-server-5.6
+sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+sudo service mysql restart
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
+echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
