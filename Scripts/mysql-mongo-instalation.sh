2e25147ae34c98586e88abab3e473b9691312485
harish-kumar-garg <harish.garg@mindtree.com>
2016-09-28 16:49:53 +0530

    Update mysql-mongo-instalation.sh
    update

--- Scripts/mysql-mongo-instalation.sh
+++ Scripts/mysql-mongo-instalation.sh
@@ -16,10 +16,12 @@ echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE U
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
+set +e
 sudo apt-get install -y mongodb-org=3.2.9 mongodb-org-server=3.2.9 mongodb-org-shell=3.2.9 mongodb-org-mongos=3.2.9 mongodb-org-tools=3.2.9
-# echo "mongodb-org hold" | sudo dpkg --set-selections
-# echo "mongodb-org-server hold" | sudo dpkg --set-selections
-# echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-# echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-# echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+set -e
 sudo service mongod start
