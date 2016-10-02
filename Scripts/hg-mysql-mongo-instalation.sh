b259704c192475d3939896118e3d7b3a0da10f84
harish-kumar-garg <harish.garg@mindtree.com>
2016-10-03 01:37:52 +0530

    Update hg-mysql-mongo-instalation.sh
    update

--- Scripts/hg-mysql-mongo-instalation.sh
+++ Scripts/hg-mysql-mongo-instalation.sh
@@ -13,6 +13,7 @@ echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flus
 echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
 
 # installing Mongo DATABASE
+sudo su
 # Configure mongodb.list file with the correct location
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
@@ -33,4 +34,4 @@ sudo apt-get install -y mongodb-org
 
 # Uncomment this to bind to all ip addresses
 sudo sed -i -e 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
-sudo service mongod start
+sudo service mongod restart
