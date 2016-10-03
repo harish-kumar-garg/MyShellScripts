cae6b55cc1b71393a7d4e4b0bb69c6899e83f3ab
harish-kumar-garg <harish.garg@mindtree.com>
2016-10-03 17:12:12 +0530

    Update hg-mysql-mongo-instalation.sh
    update

--- Scripts/hg-mysql-mongo-instalation.sh
+++ Scripts/hg-mysql-mongo-instalation.sh
@@ -11,26 +11,17 @@ sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mys
 sudo service mysql restart
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
 echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
-
 # installing Mongo DATABASE
 # Configure mongodb.list file with the correct location
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-
 # Disable THP
 sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
 sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
 sudo grep -q -F 'transparent_hugepage=never' /etc/default/grub || echo 'transparent_hugepage=never' >> /etc/default/grub
-
 # Install updates
 sudo apt-get -y update
-
 # Modified tcp keepalive according to https://docs.mongodb.org/ecosystem/platforms/windows-azure/
 sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
-
 #Install Mongo DB
 sudo apt-get install -y mongodb-org
-
-# Uncomment this to bind to all ip addresses
-# sudo sed -i -e 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
-# sudo service mongod restart
