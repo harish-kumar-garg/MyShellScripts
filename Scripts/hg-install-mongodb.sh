597985a6596024f28613e0f771e2983a24dd7fc3
harish-kumar-garg <harish.garg@mindtree.com>
2016-10-27 19:28:14 +0530

    Create hg-install-mongodb.sh
    install mongo db

---
+++ Scripts/hg-install-mongodb.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+apt-get update -y
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo 'deb http://repo.mongodb.org/apt/ubuntu '$(lsb_release -sc)'/mongodb-org/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-get update -y
+sudo apt-get install -y mongodb-org
+mongod --version
+service mongod start
